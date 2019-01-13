796d90276ce7fd69f4bad77c3f8733310ee2609b
Renan Diego <renan.ccomp@gmail.com>
2019-01-12 23:02:50 -0300

    Referenciando os posts do usuário

--- src/main/java/com/renandiego/workshopmongo/config/Instantiation.java
+++ src/main/java/com/renandiego/workshopmongo/config/Instantiation.java
@@ -43,6 +43,9 @@ public class Instantiation implements CommandLineRunner{
 		Post post2 = new Post(null, sdf.parse("20/04/2018"), "Voltei de viagem", "Voltei de São Paulo", new AuthorDTO(maria));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
 	}
 
 }
